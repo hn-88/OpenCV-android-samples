47a35f946aa26c6f650156a15dafa6422886273f
hn-88 <hari@sssmediacentre.org>
2025-05-05 14:42:12 +0530

    adding build-logic dir

---
+++ build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
@@ -0,0 +1,12 @@
+package com.android.ndk.samples.buildlogic
+
+import org.gradle.api.JavaVersion
+
+object Versions {
+    const val COMPILE_SDK = 34
+    const val TARGET_SDK = 34
+    const val MIN_SDK = 21
+    const val NDK = "27.1.12297006" // r27b
+    const val CMAKE = "3.22.1"
+    val JAVA = JavaVersion.VERSION_1_8
+}
